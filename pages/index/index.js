3d65b7bfff96f57d0eeb958f9e71eba9e4133662
thisang <thisang@foxmail.com>
2019-11-28 21:05:49 +0800

    添加分享和查看上一条

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,13 +3,8 @@ var data = require('../../data/json');
 Page({
   data: {
     theText: 'loading',
+    lastOne: null,
     dataArr: null,
-  },
-  //事件处理函数
-  handleClick() {
-    this.setData({
-      theText: this.get1fk()
-    })
   },
   onLoad: function() {
     this.setData({
@@ -23,9 +18,21 @@ Page({
   },
   onPullDownRefresh() {
     this.setData({
+      lastOne: this.data.theText,
       theText: this.get1fk()
     })
     wx.stopPullDownRefresh();
+  },
+  onShareAppMessage() {
+    wx.showShareMenu({
+      withShareTicket: false
+    })
+  },
+  handleClick() {
+    this.setData({
+      lastOne: this.data.theText,
+      theText: this.get1fk()
+    })
   },
   get1fk() {
     return this.data.arr[this.getRandomIntInclusive(0, this.data.arr.length)];
@@ -34,5 +41,11 @@ Page({
     min = Math.ceil(min);
     max = Math.floor(max);
     return  Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  handleViewLast() {
+    this.setData({
+      theText: this.data.lastOne,
+      lastOne: null
+    })
   }
 })
